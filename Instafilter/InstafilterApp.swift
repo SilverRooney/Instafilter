9a4f3d7ac54daefeffd8a3037eb10ed40d19e651
Silver Rooney <silver.rooney.dev@gmail.com>
2022-07-21 14:03:16 -0700

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Silver on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
