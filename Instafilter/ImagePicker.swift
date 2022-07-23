2f74588cf91c3062b29c4f501f3ffc49717ad929
Silver Rooney <silver.rooney.dev@gmail.com>
2022-07-23 13:31:00 -0700

    coordinators to manage SwiftUI view controllers
    coordinators as a bridge for swiftui and uikit for photos

--- Instafilter/ImagePicker.swift
+++ Instafilter/ImagePicker.swift
@@ -9,6 +9,27 @@ import PhotosUI
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else { return }
+            
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+    }
+    
     func makeUIViewController(context: Context) -> PHPickerViewController {
         //create a new picker
         var config = PHPickerConfiguration()
@@ -17,11 +38,16 @@ struct ImagePicker: UIViewControllerRepresentable {
         
         //put that into a view controller
         let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
         //and send it back
         return picker
     }
     
     func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
         
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
